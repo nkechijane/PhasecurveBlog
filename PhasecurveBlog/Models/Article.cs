329393fcc8f82442ad542ceb42b25b9b740118c6
Okeke Nkechi Jane <janecutie89@gmail.com>
2023-01-10 22:18:54 +0100

    Adding Article Model

---
+++ PhasecurveBlog/Models/Article.cs
@@ -0,0 +1,13 @@
+namespace PhasecurveBlog.Models;
+
+public class Article
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Body { get; set; } = "";
+    public string Images { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Tags { get; set; } = "";
+    public string Category { get; set; } = "";
+    public DateTime Created { get; set; } = DateTime.Now;
+}
