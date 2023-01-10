648101e690b721352810953049ac1cfd5a107a7a
Okeke Nkechi Jane <janecutie89@gmail.com>
2023-01-10 19:32:18 +0100

    Initial Commit

---
+++ PhasecurveBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PhasecurveBlog.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
