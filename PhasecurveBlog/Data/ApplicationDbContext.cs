92114d6ec2cba383cdc3a207a006f14dbf9eda1a
Okeke Nkechi Jane <janecutie89@gmail.com>
2023-01-10 22:19:56 +0100

    Adding Migration

--- PhasecurveBlog/Data/ApplicationDbContext.cs
+++ PhasecurveBlog/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PhasecurveBlog.Models;
 
 namespace PhasecurveBlog.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<Article> Articles { get; set; }
 }
