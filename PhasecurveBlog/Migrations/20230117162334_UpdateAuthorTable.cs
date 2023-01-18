01d6d19cc44dfcdfac782400a0e85d5bacdce654
Okeke Nkechi Jane <janecutie89@gmail.com>
2023-01-18 11:07:43 +0100

    Adding Migration to Update Author Table

---
+++ PhasecurveBlog/Migrations/20230117162334_UpdateAuthorTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhasecurveBlog.Migrations
+{
+    public partial class UpdateAuthorTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Authors_AuthorId",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_AuthorId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Articles");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegisteredTime",
+                table: "Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegisteredTime",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Authors");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Articles",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_AuthorId",
+                table: "Articles",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Authors_AuthorId",
+                table: "Articles",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id");
+        }
+    }
+}
